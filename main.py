6506c0e05584a5d3c720a6b9108a80d9fc7c0f1a
RandBey <galichyanr@gmail.com>
2022-10-21 21:03:35 +0300

    Implemented base functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 import settings
 import time
 
+start_time = float()
+end_time = float()
 # app_window configuration
 
 app_win = tk.Tk()
@@ -12,6 +14,23 @@ app_win.configure(bg=settings.bg_color)
 
 # Placing widgets
 
+def show_score():
+    time_spend = end_time - start_time
+    time_label['text'] = f'time: {time_spend:0.2f}secs  '
+    
+    target = text_label['text']
+    enter = text_entry.get()
+    coincidence = 0
+    for i in range(len(target)):
+        if enter[i] == target[i]: coincidence += 1
+    accuracy = (coincidence * 100) // len(target)
+    accuracy_label['text'] = f'Accuracy: {accuracy}%  '
+
+    wpm = (len(enter) / time_spend) * 60
+    wpm_label['text'] = f'Wpm: {wpm:0.2f}  '
+
+    info_panel.pack(side=tk.BOTTOM)
+
 title = tk.Label(app_win, text='Typing Speed Test', foreground=settings.font_color, 
                 background=app_win['bg'], font=settings.title_font)
 title.pack(side=tk.TOP, pady=40)
@@ -20,32 +39,54 @@ text_label = tk.Label(app_win, text='Tap the button to start', foreground=settin
                 background=app_win['bg'], font=settings.text_font)
 text_label.pack(side=tk.TOP, pady=10)
 
+def comparison(event): 
+    global end_time
+    if len(text_label['text']) == len(text_entry.get()):
+        end_time = time.perf_counter()
+        text_entry['state'] = 'disabled'
+        app_win.focus()
+        show_score()
+
 text_entry = tk.Entry(app_win, width=50, font=settings.input_font, bg=app_win["bg"],
-                      justify=tk.CENTER, bd=10, relief=tk.FLAT, highlightcolor=settings.font_color)
+                      justify=tk.CENTER, bd=10, relief=tk.FLAT, highlightcolor=settings.font_color,
+                      state= "disabled", disabledbackground=app_win["bg"], disabledforeground=settings.dis_color)
 text_entry.pack(side=tk.TOP, pady=20)
+text_entry.bind('<KeyPress>',comparison)
 
-def countdown(count=3):
+def countdown(count=settings.countdown):
     label_counter['text'] = count
-
     if count > 0:
         app_win.after(1000, countdown, count-1)
     else: 
         label_counter['text'] = ''
         start_typing()
 
-
 def start_typing():
+    global start_time
+    text_entry['state'] = "normal"
+    text_entry.delete(0,tk.END)
+    info_panel.pack_forget()
     sentence = 'A Brown Fox Jumped Over The Lazy Dogs quickly'
+    start_time = time.perf_counter()
     text_label['text'] = sentence
+    text_entry.focus()
 
 
-        
 start_button = tk.Button(app_win, text = 'Start', width=8, height=2, bd=0, command=countdown)
 start_button.pack(side=tk.BOTTOM, pady=20)
 label_counter = tk.Label(app_win, foreground=settings.font_color, 
                 background=app_win['bg'], font=settings.title_font)
 label_counter.pack(side=tk.BOTTOM)
-
+info_panel = tk.PanedWindow(orient ='horizontal')
+time_label = tk.Label(info_panel, foreground=settings.font_color, 
+                background=app_win['bg'], font=settings.text_font)
+accuracy_label = tk.Label(info_panel, foreground=settings.font_color, 
+                background=app_win['bg'], font=settings.text_font)
+wpm_label = tk.Label(info_panel, foreground=settings.font_color, 
+                background=app_win['bg'], font=settings.text_font)
+info_panel.add(time_label)
+info_panel.add(accuracy_label)
+info_panel.add(wpm_label)
 
 
 
--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 bg_color = "#2b2d42"
 font_color = "#EDF2F4"
+dis_color = "#8D99AE"
 red_col = "#EF233C"
 window_size = "700x500"
 title_font = ("Helvetica", 50, "bold")
 text_font = ("Helvetica", 20, "normal")
 input_font = ("Helvetica", 17, "normal")
+countdown = 3
