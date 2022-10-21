4b796a513f312b0fa553897734eed5730a6e5cb2
RandBey <galichyanr@gmail.com>
2022-10-21 18:49:52 +0300

    start typing function now start after countdown

--- main.py
+++ main.py
@@ -24,26 +24,28 @@ text_entry = tk.Entry(app_win, width=50, font=settings.input_font, bg=app_win["b
                       justify=tk.CENTER, bd=10, relief=tk.FLAT, highlightcolor=settings.font_color)
 text_entry.pack(side=tk.TOP, pady=20)
 
-def countdown(count):
+def countdown(count=3):
     label_counter['text'] = count
 
     if count > 0:
         app_win.after(1000, countdown, count-1)
-    else: label_counter['text'] = ''
+    else: 
+        label_counter['text'] = ''
+        start_typing()
 
 
 def start_typing():
-    countdown(3)
-    
+    sentence = 'A Brown Fox Jumped Over The Lazy Dogs quickly'
+    text_label['text'] = sentence
 
 
         
-
-start_button = tk.Button(app_win, text = 'Start', width=8, height=2, bd=0, command=start_typing)
+start_button = tk.Button(app_win, text = 'Start', width=8, height=2, bd=0, command=countdown)
 start_button.pack(side=tk.BOTTOM, pady=20)
 label_counter = tk.Label(app_win, foreground=settings.font_color, 
                 background=app_win['bg'], font=settings.title_font)
 label_counter.pack(side=tk.BOTTOM)
+
 
 
 
