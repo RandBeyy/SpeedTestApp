b3f85e9e806b37ceff4549e442f29270526fd606
RandBey <galichyanr@gmail.com>
2022-10-21 18:26:55 +0300

    implemented counter before start typing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import settings
+import time
 
 # app_window configuration
 
@@ -23,12 +24,26 @@ text_entry = tk.Entry(app_win, width=50, font=settings.input_font, bg=app_win["b
                       justify=tk.CENTER, bd=10, relief=tk.FLAT, highlightcolor=settings.font_color)
 text_entry.pack(side=tk.TOP, pady=20)
 
+def countdown(count):
+    label_counter['text'] = count
+
+    if count > 0:
+        app_win.after(1000, countdown, count-1)
+    else: label_counter['text'] = ''
+
+
 def start_typing():
-    return
+    countdown(3)
+    
 
-start_button = tk.Button(app_win, text = 'Start', width=8, height=2, bd=0)
+
+        
+
+start_button = tk.Button(app_win, text = 'Start', width=8, height=2, bd=0, command=start_typing)
 start_button.pack(side=tk.BOTTOM, pady=20)
-
+label_counter = tk.Label(app_win, foreground=settings.font_color, 
+                background=app_win['bg'], font=settings.title_font)
+label_counter.pack(side=tk.BOTTOM)
 
 
 
