4a362c0101badf52d85995fed1c51773cef4e6b7
RandBey <galichyanr@gmail.com>
2022-10-22 20:08:03 +0300

    change settings into class

--- settings.py
+++ settings.py
@@ -1,9 +1,15 @@
-bg_color = "#2b2d42"
-font_color = "#EDF2F4"
-dis_color = "#8D99AE"
-red_col = "#EF233C"
-window_size = "700x500"
-title_font = ("Helvetica", 50, "bold")
-text_font = ("Helvetica", 20, "normal")
-input_font = ("Helvetica", 17, "normal")
-countdown = 3
+class Settings:
+    def __init__(self):
+
+        # Main window settings:
+        self.window_size = "700x500"
+        self.bg_color = "#2b2d42"
+    
+    
+        self.font_color = "#EDF2F4"
+        self.dis_color = "#8D99AE"
+        self.red_col = "#EF233C"
+        self.title_font = ("Helvetica", 50, "bold")
+        self.text_font = ("Helvetica", 20, "normal")
+        self.input_font = ("Helvetica", 17, "normal")
+        self.countdown = 3
